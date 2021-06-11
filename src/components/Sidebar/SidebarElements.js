4d17bc507e9da8c90f304f29d61f27f6bf4f1a8c
sammycaesar <samantha.caesar@bigpond.com>
2021-06-11 14:40:55 +0200

    first commit 😄😋

---
+++ src/components/Sidebar/SidebarElements.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import { Link as LinkS } from "react-scroll";
+import { FaTimes } from "react-icons/fa";
+
+export const SidebarContainer = styled.aside`
+  position: fixed;
+  z-index: 999;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  align-items: center;
+  top: 0;
+  left: 0;
+  transition: 1s ease-in-out;
+  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+  top: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
+`;
+
+export const ClosedIcon = styled(FaTimes)`
+  color: #fff;
+`;
+
+export const Icon = styled.div`
+  position: absolute;
+  top: 1.2rem;
+  right: 1.5rem;
+  background: transparent;
+  font-size: 2rem;
+  cursor: pointer;
+  outline: none;
+`;
+
+export const SidebarWrapper = styled.div`
+  color: #fff;
+`;
+
+export const SidebarMenu = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(4, 150px);
+  text-align: center;
+
+  @media screen and (max-width: 480px) {
+    grid-template-rows: repeat(4, 100px);
+  }
+`;
+
+export const SidebarLink = styled(LinkS)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 4rem;
+  text-decoration: none;
+  list-style: none;
+  transition: 0.2s ease-in-out;
+  text-decoration: none;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    color: #fa9182;
+    transition: 0.2 ease-in-out;
+  }
+`;
